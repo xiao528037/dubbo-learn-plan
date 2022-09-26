a887192e3f8112f7a323ed68b38c1dfe635c378b
lucky <aloneman0629@gmail.com>
2022-09-26 21:37:00 +0800

    1

---
+++ src/main/java/com/xiao/dubbolearnplan/DubboLearnPlanApplication.java
@@ -0,0 +1,13 @@
+package com.xiao.dubbolearnplan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DubboLearnPlanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DubboLearnPlanApplication.class, args);
+    }
+
+}
